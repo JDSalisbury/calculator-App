fa6779b74406614cc7c907c5c0acaa88f5b96553
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-08-02 11:23:07 -0400

    percent works

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -3,7 +3,7 @@
         <div class="display">{{current || '0'}}</div>
         <div @click="clear" class="btn">Clear</div>
         <div @click="sign" class="btn">+/-</div>
-        <div class="btn">%</div>
+        <div @click="percent" class="btn">%</div>
         <div class="btn operator">/</div>
         <div class="btn">7</div>
         <div class="btn">8</div>
@@ -39,6 +39,9 @@ export default {
     sign() {
         this.current = this.current.charAt(0) === "-" ?
         this.current.slice(1) : `-${this.current}`;
+    },
+    percent() {
+        this.current = `${parseFloat(this.current) / 100}`
     }
   }
 };
